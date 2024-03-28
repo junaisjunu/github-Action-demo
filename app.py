183ee80f2988cb149f8a38beb7e375b308a298e5
junais <junaisk456@gmail.com>
2024-03-28 16:29:32 +0530

    ff

---
+++ app.py
@@ -0,0 +1,15 @@
+
+
+class app:
+    def __init__(self) -> None:
+        pass
+
+    def game(self,a,b):
+        return a+b
+    
+
+
+if __name__=="__main__":
+    ap=app()
+    print(ap.game(12,13))
+        
